65eb5ab8373a1b6ea9b8861bc19091fb651a4a37
sdfy-fly <rozhov25@bk.ru>
2023-06-29 21:27:56 +0400

    client: header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import './Header.css'
+import logo from '../../assets/logo.svg'
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <div className="header__wrapper">
+                <img src={logo} alt="Logo"/>
+                <div className="header__links">
+                    <p>О проекте</p>
+                    <p>Расписание</p>
+                    <p>Команды</p>
+                    <p>Организаторы</p>
+                    <p>Контакты</p>
+                    <p className="header__links__register">Стать участником</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Header
