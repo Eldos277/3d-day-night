0150aa79adc78dd296e3df78e66145dcf9a8319c
sdfy-fly <rozhov25@bk.ru>
2023-06-29 21:33:36 +0400

    client: react router dom

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Main from "../../pages/Main/Main";
+import {Route, Routes} from "react-router-dom";
+import ErrorPage from "../../pages/ErrorPage/ErorPage";
 
 function App() {
-  return (
-    <Main/>
-  );
+    return (
+        <Routes>
+            <Route path='/' element={<Main/>}/>
+            <Route path='*' element={<ErrorPage/>}/>
+        </Routes>
+    );
 }
 
 export default App;
