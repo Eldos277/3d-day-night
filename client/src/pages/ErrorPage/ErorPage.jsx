6b9f548f5b0f3d428c69e2a648a1249eb549d26e
sdfy-fly <rozhov25@bk.ru>
2023-06-29 21:33:56 +0400

    client: error page

---
+++ client/src/pages/ErrorPage/ErorPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+
+const ErrorPage = () => {
+
+    return (
+        <>
+            <Header></Header>
+            <div>Error page</div>
+        </>
+    )
+}
+
+export default ErrorPage
