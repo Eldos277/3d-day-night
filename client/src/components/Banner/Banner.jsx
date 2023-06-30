5f783a560782851a1fe708d707134291747d25f1
sdfy-fly <rozhov25@bk.ru>
2023-06-30 20:25:42 +0400

    client: banner

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import './Banner.css'
+import logo_3d from '../../assets/3d_day_logo.png'
+
+const Banner = () => {
+
+    return (
+        <div className="banner">
+            <div className="banner__wrapper">
+                <div className="banner__wrapper__logo">
+                    <img src={logo_3d} alt="3d-day-logo"/>
+                </div>
+
+                <div className="banner__wrapper__text">
+                    <h2>Всероссийский конкурс
+                        по скоростному трехмерному
+                        моделированию и визуализации
+                    </h2>
+                    <h1>3D день и 3D ночь</h1>
+                </div>
+            </div>
+            <div className="banner_wrapper__footer">
+                <h4>Саратов, 1-14 июня 2023</h4>
+            </div>
+        </div>
+    )
+
+}
+
+export default Banner
