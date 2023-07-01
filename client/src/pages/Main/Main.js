b3d35bb499b1dc7e5939e0dfde34ee629caf53cc
sdfy-fly <rozhov25@bk.ru>
2023-07-01 18:23:18 +0400

    client: main: about

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner/Banner";
+import About from "../../components/About/About";
 
 const Main = () => {
 
     return (
         <>
             <Banner/>
+            <About/>
         </>
     )
 }
