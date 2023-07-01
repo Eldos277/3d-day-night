7bf92d0c879a58602711aee4c4c656467d2a2ea6
sdfy-fly <rozhov25@bk.ru>
2023-07-01 18:22:49 +0400

    client: home link

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,26 @@
 import './Header.css'
 import logo from '../../assets/images/logo.svg'
+import {useNavigate} from "react-router-dom";
 
 const Header = () => {
+
+    const navigate = useNavigate()
+
+    const handleLogoClick = () => {
+        navigate('/')
+    }
 
     return (
         <div className="header">
             <div className="header__wrapper">
-                <img src={logo} alt="Logo"/>
+                <img src={logo} className="header_link" alt="Logo" onClick={handleLogoClick}/>
                 <div className="header__links">
-                    <p>О проекте</p>
-                    <p>Расписание</p>
-                    <p>Команды</p>
-                    <p>Организаторы</p>
-                    <p>Контакты</p>
-                    <p className="header__links__register">Стать участником</p>
+                    <p className="header_link">О проекте</p>
+                    <p className="header_link">Расписание</p>
+                    <p className="header_link">Команды</p>
+                    <p className="header_link">Организаторы</p>
+                    <p className="header_link">Контакты</p>
+                    <p className="header__links__register header_link">Стать участником</p>
                 </div>
             </div>
         </div>
