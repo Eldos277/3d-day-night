f676485b1149298aefcc865fdce0d3ee3240d44b
sdfy-fly <rozhov25@bk.ru>
2023-07-03 22:18:16 +0400

    client: schedule

--- client/src/components/Schedule/Schedule.jsx
+++ client/src/components/Schedule/Schedule.jsx
@@ -8,7 +8,7 @@ const Schedule = () => {
     const [activeTab, setActiveTab] = useState(true)
 
     const handleSwitcher = (e) => {
-        if (e.target.innerText === 'Архитектурная визуализация')
+        if (e.target.innerText === 'Архитектурная\n' + 'визуализация')
             setActiveTab(true)
         else
             setActiveTab(false)
@@ -22,8 +22,12 @@ const Schedule = () => {
             <div className="schedule__wrapper">
 
                 <div className="schedule__wrapper__switcher">
-                    <button onClick={handleSwitcher} className={activeTab ? 'active' : ''}>Архитектурная визуализация</button>
-                    <button onClick={handleSwitcher} className={activeTab ? '' : 'active'}>Персонажное моделирование</button>
+                    <button onClick={handleSwitcher} className={activeTab ? 'active' : ''}>
+                        <h5>Архитектурная <br/> визуализация</h5>
+                    </button>
+                    <button onClick={handleSwitcher} className={activeTab ? '' : 'active'}>
+                        <h5>Персонажное <br/> моделирование</h5>
+                    </button>
                 </div>
 
                 <Scheduler activeTab={activeTab}/>
